887137fa3e7f3937a3cd308130b1ed9f1a02061c
kezymz <144817081+kezymz@users.noreply.github.com>
2023-09-26 14:41:29 +0100

    Add files via upload

---
+++ PythonApplication2.py
@@ -0,0 +1,9 @@
+print("hello my name is susan from space")
+name = input("what is your name?")
+print(f"hello {name} i am from the year 2210 and i am 20 years old")
+age = input("how old are you?")
+future_age = int(age) + (2210-2020)
+print(f"wow by 2210 you will be {future_age}. that is really old")
+music = input("what music are you into?")
+print(f"i have not heard of {music} ")
+
